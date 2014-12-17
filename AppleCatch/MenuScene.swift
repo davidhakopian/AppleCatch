ad05cf5fa2ea0b68615ec0d9fd972166c99d6d1f
davidhakopian <david.hakopian@mail.mcgill.ca>
2014-12-16 22:41:48 -0500

    GameMenuScene create

---
+++ AppleCatch/MenuScene.swift
@@ -0,0 +1,15 @@
+//
+//  MenuScene.swift
+//  AppleCatch
+//
+//  Created by David Hakopian on 2014-12-16.
+//  Copyright (c) 2014 David Hakopian. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MenuScene: SKScene {
+    
+    
+}
